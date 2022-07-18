26a474d610d603ca18ff58bf4460b3cfc5083c84
Titrom25 <titrom25@mail.ru>
2022-07-18 15:18:14 +0700

    Improve logging

--- recorder.py
+++ recorder.py
@@ -1,44 +1,53 @@
 import cv2
-import logging
-import logging.handlers as handlers
 import os
 import time
 import threading as th  
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 ROOT_PATH = '/records'
 LOG_PATH = '/logs'
+LOG_FILE = ''
 CAMERA_NAME = VIDEO_DIR = None
-LOGGER = None
 
 NEXT_VIDEO = False
 VIDEO_MINUTES = 3
 
 
-def initLogger():
-	global LOGGER
-	logging.basicConfig(level=logging.INFO,
-						format='[%(asctime)s] %(levelname)s: %(message)s',
-						datefmt='%d-%m-%y %H:%M:%S')
-	LOGGER = logging.getLogger('recorder')
-	logHandler = handlers.TimedRotatingFileHandler(f'/{LOG_PATH}/cameras.log', when='H', interval=1)
-	formatter = logging.Formatter('[%(asctime)s] %(levelname)s: %(message)s')
-	logHandler.setFormatter(formatter)
-	logHandler.setLevel(logging.INFO)
-	logHandler.suffix = '%d-%m-%y_%H-%M'
-	LOGGER.addHandler(logHandler)
+def videoLimitExceeded(dirpath):
+	if not os.path.exists(dirpath):
+		return False
+	return len([
+			s for s in os.listdir(dirpath)
+			if os.path.isfile(os.path.join(dirpath, s)) 
+			and os.path.splitext(s)[1] == '.ts'
+		]) >= 20
+
+
+def logMessage(type, message):
+	tries = 0
+	while tries <= 5:
+		try:
+			with open(f'{LOG_PATH}/{LOG_FILE}', 'a') as logFile:
+				timestamp = datetime.now().strftime('%d-%m-%y %H:%M:%S')
+				logFile.write(f'[{timestamp}] {type}: {message}\n')
+			break
+		except Exception:
+			tries += 1
+			time.sleep(0.01)
+			pass
 
 
 class RTSPVideoWriterObject(object):
 	def __init__(self, src):
 		self.src = src
 		self.capture = cv2.VideoCapture(self.src)
-		self.reconnect_time = 1
-
-		self.fps = int(self.capture.get(5))
 		self.frame_width = 800
 		self.frame_height = 450
+
+		self.reconnect_time = 1
+
+		self.fps = int(self.capture.get(cv2.CAP_PROP_FPS))
 		self.codec = cv2.VideoWriter_fourcc(*'h264')
 
 		self.frame_count = 1
@@ -48,13 +57,13 @@ class RTSPVideoWriterObject(object):
 
 	def reconnect(self):
 		if self.reconnect_time == 1:
-			LOGGER.warning(f'Connection lost for camera {CAMERA_NAME} - sleep for a {self.reconnect_time} seconds and reconnecting...')
+			logMessage('WARNING', f'Connection lost for camera {CAMERA_NAME} - sleep for a {self.reconnect_time} seconds and reconnecting...')
 		else:
-			LOGGER.warning(f'Connection failed for camera {CAMERA_NAME} - sleep for a {self.reconnect_time} seconds and reconnecting...')
+			logMessage('WARNING', f'Connection failed for camera {CAMERA_NAME} - sleep for a {self.reconnect_time} seconds and reconnecting...')
 		time.sleep(self.reconnect_time)
 		self.capture = cv2.VideoCapture(self.src)
 		if self.capture.isOpened():
-			LOGGER.info(f'Reconnect for camera {CAMERA_NAME} - SUCCESS')
+			logMessage('INFO', f'Reconnect for camera {CAMERA_NAME} - SUCCESS')
 			self.reconnect_time = 1
 		else:
 			self.reconnect_time *= 2
@@ -86,16 +95,23 @@ class RTSPVideoWriterObject(object):
 				os.system(f'mv "{self.current_videoname}" "{saved_videoname}"')
 
 				self.current_videoname = f'{VIDEO_DIR}/{str(round(time.time() * 1000))}_0_.ts'
-				LOGGER.info(f'New video ({saved_videoname.split("/")[-1]}) saved for camera {CAMERA_NAME}')
+				logMessage('INFO', f'New video ({saved_videoname.split("/")[-1]}) saved for camera {CAMERA_NAME}')
 
+				self.output_video.release()
 				self.output_video = cv2.VideoWriter(self.current_videoname, self.codec, self.fps, (self.frame_width, self.frame_height))
 				NEXT_VIDEO = False
 
 
 def updateVideoDir():
 	global VIDEO_DIR
+	global LOG_FILE
 	year, month, day, hour = datetime.now().strftime("%Y %m %d %H").split(' ')
 	VIDEO_DIR = f'{ROOT_PATH}/{CAMERA_NAME}/vid/{year}/{month}/{day}/{hour}'
+	if videoLimitExceeded(VIDEO_DIR):
+		year, month, day, hour = (datetime.now() + timedelta(hours=1)).strftime("%Y %m %d %H").split(' ')
+		VIDEO_DIR = f'{ROOT_PATH}/{CAMERA_NAME}/vid/{year}/{month}/{day}/{hour}'
+	
+	LOG_FILE = f'log_{year}_{month}_{day}_{hour}.log'
 	os.makedirs(VIDEO_DIR, exist_ok=True)
 
 
@@ -111,10 +127,14 @@ if __name__ == '__main__':
 	CAMERA_NAME = os.environ['CAMERA_NAME']
 	rtsp_stream_link = os.environ['STREAM_LINK']
 
-	initLogger()
-	LOGGER.info(f'Start recorder for camera {CAMERA_NAME}')
-
+	try:
+		os.makedirs(LOG_PATH, exist_ok=True)
+	except Exception:
+		pass
+	
 	updateVideoDir()
+	logMessage('INFO', f'Start recorder for camera {CAMERA_NAME}')
+
 	timerThread = th.Thread(target=timer)  
 	timerThread.daemon = True
 	timerThread.start()  
@@ -124,5 +144,4 @@ if __name__ == '__main__':
 		try:
 			video_stream_widget.startRecorder()
 		except Exception as e:
-			print("KeyboardInterrupt received.")
-			LOGGER.error(e)
+			logMessage('ERROR', e)
